c63e35c8c992aac5dd667201105b67506902ca43
otttisss <otttisss@yandex.ru>
2023-10-06 13:18:38 +0300

    exercise1 completed

--- Day00/src/exercise1/src/main/kotlin/com/example/kotlincourse/Circles.kt
+++ Day00/src/exercise1/src/main/kotlin/com/example/kotlincourse/Circles.kt
@@ -1,8 +1,44 @@
 package com.example.kotlincourse
 
-class Circles {
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+fun main() {
+    val x1 = getInput("x1")
+    val y1 = getInput("y1")
+    val r1 = getInput("r1")
+    val x2 = getInput("x2")
+    val y2 = getInput("y2")
+    val r2 = getInput("r2")
+
+    val distance = sqrt((x2 - x1).pow(2) + (y2 - y1).pow(2))
+
+    when {
+        distance < r1 + r2 -> {
+            if (distance > kotlin.math.abs(r2 - r1)) {
+                println("Result: the circles intersect")
+            } else if (distance == kotlin.math.abs(r2 - r1)) {
+                println("Result: the circles touch")
+            } else {
+                println("Result: one circle is inside another")
+            }
+        }
+        distance == r1 + r2 -> {
+            println("Result: the circles touch")
+        }
+        else -> {
+            println("Result: the circles do not intersect")
+        }
+    }
 }
 
-fun main(args: Array<String>) {
-    println("hellloooooooooo")
+fun getInput(prompt: String): Double {
+    while (true) {
+        print("Input $prompt: ")
+        try {
+            return readLine()?.toDouble() ?: throw NumberFormatException()
+        } catch (e: NumberFormatException) {
+            println("Couldn't parse a number. Please, try again")
+        }
+    }
 }
